ea28bedb88845c2fe9a5a79f2b8327b895ac1c6f
Pavel Chaplygin <mail@pchapl.dev>
2023-02-12 20:47:24 +0200

    Debug

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-echo -e "${PRIVATE_KEY}" >_TEMP_IDENTITY_FILE && \
+echo $PATH
+
+echo "$PRIVATE_KEY" >_TEMP_IDENTITY_FILE && \
 chmod 600 _TEMP_IDENTITY_FILE && \
 ls -la && \
 scp -o StrictHostKeyChecking=no -v -i _TEMP_IDENTITY_FILE -r 'build' deployer@cookbook.pchapl.dev:/home/deployer/CookBookFrontend/releases && \
-ssh deployer@cookbook.pchapl.dev "./release-react.sh"
+ssh -v -i _TEMP_IDENTITY_FILE deployer@cookbook.pchapl.dev "./release-react.sh"
