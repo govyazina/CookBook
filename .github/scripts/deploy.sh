ff62dbb42e9f57a3d92a82bd873dc2410e5fe5b6
Pavel Chaplygin <mail@pchapl.dev>
2023-02-12 20:49:19 +0200

    Deploy: finally

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-echo $PATH
-
-echo "$PRIVATE_KEY" >_TEMP_IDENTITY_FILE && \
+echo -e "${PRIVATE_KEY}" >_TEMP_IDENTITY_FILE && \
 chmod 600 _TEMP_IDENTITY_FILE && \
 ls -la && \
 scp -o StrictHostKeyChecking=no -v -i _TEMP_IDENTITY_FILE -r 'build' deployer@cookbook.pchapl.dev:/home/deployer/CookBookFrontend/releases && \
